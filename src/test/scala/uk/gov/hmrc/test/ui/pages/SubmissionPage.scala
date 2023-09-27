9f0fa340c1e2d49bcaf3cc4f1051b7d62364fda2
lahiru.wijesinghe@opencastsoftware.com <lahiru.wijesinghe@opencastsoftware.com>
2023-09-27 20:57:43 +0100

    uncomment  submission page verification

--- src/test/scala/uk/gov/hmrc/test/ui/pages/SubmissionPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/SubmissionPage.scala
@@ -16,10 +16,12 @@
 
 package uk.gov.hmrc.test.ui.pages
 
+import uk.gov.hmrc.test.ui.constants.PageInformation.{SUBMISSION_PAGE_HEADER, SUBMISSION_PAGE_TITLE}
+
 object SubmissionPage extends BasePage {
   def verifySubmissionPage() = {
-    /* verifyPageUrl("submission")
+    verifyPageUrl("submission")
     onPage(SUBMISSION_PAGE_TITLE)
-    isHeader(SUBMISSION_PAGE_HEADER)*/
+    isHeader(SUBMISSION_PAGE_HEADER)
   }
 }
