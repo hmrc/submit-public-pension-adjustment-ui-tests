81b626e48680d915d14da4bce078c36dfec29ed6
lahiru.wijesinghe@opencastsoftware.com <lahiru.wijesinghe@opencastsoftware.com>
2023-11-28 11:00:52 +0000

    update xpath of the CYA continue button

--- src/test/scala/uk/gov/hmrc/test/ui/pages/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/BasePage.scala
@@ -81,7 +81,7 @@ trait BasePage extends BrowserDriver with GSDataCollector with AASDataCollector 
     driver.findElement(By.xpath("//button[contains(text(),'Continue')]")).click()
 
   def clickContinueButton(): Unit =
-    driver.findElement(By.xpath("//a[contains(text(),'Continue')]")).click()
+    driver.findElement(By.xpath("//button[contains(text(),'Continue')]")).click()
 
   def selectYesOption(): Unit =
     driver.findElement(By.id("value")).click()
