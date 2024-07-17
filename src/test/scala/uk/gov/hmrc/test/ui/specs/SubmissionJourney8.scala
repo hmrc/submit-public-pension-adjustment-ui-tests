d86a26e4738c13a7d3745aefcce879a2107d204d
swetaoza16 <sweta.oza@digital.hmrc.gov.uk>
2024-07-17 16:10:19 +0100

    half done

--- src/test/scala/uk/gov/hmrc/test/ui/specs/SubmissionJourney8.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/SubmissionJourney8.scala
@@ -98,6 +98,9 @@ class SubmissionJourney8 extends BaseSpec with BeforeAndAfter {
       When("I verify WhichPensionSchemeWillPayTaxReliefPage Page, select pension scheme and click continue")
       WhichPensionSchemeWillPayTaxReliefPage.verifyPageSelectPensionSchemeAndContinue("Scheme 1")
 
+      When("I verify Bank Details Page, enter bank details and click continue")
+      BankDetailsPage.verifyPageEnterBankDetailsClickContinue()
+
       CheckYourAnswersCalculationJourneyPage.clickSubmissionContinueButton()
 
       When("I verify SchemeCreditConsentPage Page select yes and continue")
